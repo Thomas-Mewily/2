e4809a406d3e4524a2d4247c15f4464d5f66dbcc
Thomas-Mewily <tstd36@yahoo.fr>
2023-08-18 03:30:54 +0200

    Order 2 (and 3 : work in progress)

--- I_know.cpp
+++ I_know.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include "console.h"
 
 using namespace std;
 
@@ -13,10 +15,10 @@ using namespace std;
 01 : this is false
 00 : I don't know
 
-111 : I   know,   that this is true 
-110 : I ignore,   that this is true 
-101 : I   know,   that I don't care 
-100 : I ignore,   that I don't care 
+111 : I   know,   that this is true
+110 : I ignore,   that this is true
+101 : I   know,   that I don't care
+100 : I ignore,   that I don't care
 011 : I   know,   that this is false
 010 : I ignore,   that this is false
 001 : I   know,   that I don't know
@@ -42,95 +44,219 @@ using namespace std;
 */
 // Thank to https://gist.github.com/toch/7ed3a1786d0ed464fd94 for the fibonacci example
 
-#define unused(x)(void)(x)
+#define unused(x) (void)(x)
 
-template <int level> class b {
-    public:
-        union {
-            class b<level-1> cond;
-            class b<level-1> do_I_care;
-            class b<level-1> care;
-        };
+template <int level>
+class b
+{
+public:
+    union
+    {
+        b<level-1> cond;
+        b<level-1> do_I_care;
+        b<level-1> care;
+    };
 
-    union {
+    union
+    {
         bool do_I_know = true;
         bool know;
     };
 
-    b<level> (b<level-1> cond, bool know) {
-        this -> cond = cond;
-        this -> know = know;
+    b<level>(b<level - 1> cond, bool know)
+    {
+        this->cond = cond;
+        this->know = know;
     }
 
-    b<level> (bool know) {
-        this -> cond = b<level-1> (know);
-        this -> know = know;
+    b<level>(bool know)
+    {
+        if constexpr (level == 1)
+        {
+            this->know = know;
+        }
+        else
+        {
+            this->know = true;
+        }
+        this->cond = b<level - 1>(know);
     }
 
-    bool operator == (b<level> b) {
-        return this -> cond == b.cond && this -> do_I_know == b.do_I_know;
+    static b<level> unknow()
+    {
+        return b<level>(b<level-1>(true), false);
     }
-    bool operator != (b<level> b) {
+
+    static b<level> dont_care()
+    {
+        return b<level>(b<level-1>(false), false);
+    }
+
+    bool is_true()
+    {
+        if constexpr (level == 1)
+        {
+            return know;
+        }else
+        {
+            return know ? cond.is_true() : false;
+        }
+    }
+
+    bool is_false()
+    {
+        if constexpr (level == 1)
+        {
+            return know == false;
+        }else
+        {
+            return know ? cond.is_false() : false;
+        }
+    }
+
+    bool operator==(b<level> b)
+    {
+        return this->cond == b.cond && this->do_I_know == b.do_I_know;
+    }
+    bool operator!=(b<level> b)
+    {
         return !(this == b);
     }
 
-    ostream & print_bool(ostream & os) {
+    ostream &print_bool(ostream &os)
+    {
         cond.print_bool(os);
         os << know;
         return os;
     }
 
-    ostream & print_string(ostream & os, bool is_value = true) 
+    ostream &print_string(ostream &os, bool is_value = true)
     {
-        if constexpr(level == 1) {
-            if (is_value) {
-                os << (know ? "true" : "false");
-                cond.print_string(os, know);
-            } else {
+        if constexpr (level == 1)
+        {
+            if (is_value)
+            {
+                os << (know ? COLOR_FOREGROUND_GREEN : COLOR_FOREGROUND_RED) << (know ? "true " : "false") << COLOR_RESET;
+            }
+            else
+            {
                 os << "because " << (know ? "I'm unaware" : "I don't care");
             }
-        } else if constexpr(level == 2) {
+        }
+        else if constexpr (level == 2)
+        {
             // let the compiler do the hardwork : changing this mess of if/else
-            if(know)
+            if (know)
             {
-                if(cond.know)
+                if (cond.know)
                 {
-                    os << "this is true";
-                }else
-                {
-                    os << "this is false";
+                    os << COLOR_FOREGROUND_GREEN << "this is true " << COLOR_RESET;
                 }
-            }else
-            {
-                if(care.know)
+                else
                 {
-                    os << "I don't care";
-                }else
-                {
-                    os << "I don't know";
+                    os << COLOR_FOREGROUND_RED << "this is false" << COLOR_RESET;
                 }
             }
-        } else {
-            if (know) {
-                os << "I   know,   that ";
-            } else {
-                os << "I ignore,   that ";
+            else
+            {
+                if (care.know)
+                {
+                    os << COLOR_FOREGROUND_MAGENTA << "I don't know " << COLOR_RESET;
+                }
+                else
+                {
+                    os << COLOR_FOREGROUND_YELLOW "I don't care " << COLOR_RESET;
+                }
             }
-            cond.print_string(os, is_value&know);
+        }
+        else
+        {
+            if (know)
+            {
+                //os << COLOR_FOREGROUND_GREEN << "I   know, " << COLOR_RESET << "  that ";
+                if (cond.know)
+                {
+                    os << COLOR_FOREGROUND_GREEN << "I  know, " << COLOR_RESET << "that ";
+                }
+                else
+                {
+                    os << COLOR_FOREGROUND_RED << "I  know," << COLOR_RESET << "!that ";
+                }
+            }
+            else
+            {
+                if (care.know)
+                {
+                    os << COLOR_FOREGROUND_MAGENTA "I !know, " << COLOR_RESET << "that ";
+                }
+                else
+                {
+                    os << COLOR_FOREGROUND_YELLOW  "I !care, " << COLOR_RESET << "that ";
+                }
+            }
+            cond.print_string(os, is_value & know);
         }
         return os;
     }
 
-    friend ostream & operator << (ostream & os, b<level> b) {
+    friend ostream &operator<<(ostream &os, b<level> b)
+    {
         b.print_bool(os);
         os << " : ";
         b.print_string(os);
         return os;
     }
 
+    b<level> operator|(b<level> a)
+    {
+        if constexpr (level == 1)
+        {
+            return b<1>(know | a.know);
+        }else
+        {
+            if (is_true() | a.is_true())
+            {
+                return true;
+            }
+            if (is_false() & a.is_false())
+            {
+                return false;
+            }
+            return b<level>::unknow();
+        }
+    }
+    b<level> operator&(b<level> a)
+    {
+        if constexpr (level == 1)
+        {
+            return b<1>(know & a.know);
+        }else
+        {
+            if (is_true() & a.is_true())
+            {
+                return true;
+            }
+            if (is_false() | a.is_false())
+            {
+                return false;
+            }
+            return b<level>::unknow();
+        }
+    }
+
+    b<level> operator!()
+    {
+        if constexpr (level == 1)
+        {
+            return b<1>(!know);
+        }else
+        {
+            return b<level>(!cond, know);
+        }
+    }
     /*
-    struct b<level> operator& (b<level> b) 
-    { 
+    struct b<level> operator& (b<level> b)
+    {
       return value &  b.value;
     }
 
@@ -138,63 +264,183 @@ template <int level> class b {
     bool operator^ (struct b<level> b) { return value ^  b.value; }*/
 };
 
-template <> class b<0> {
-    public: b <0> () {}
-    b <0> (bool b) {
+template <>
+class b<0>
+{
+public:
+    b<0>() {}
+    b<0>(bool b)
+    {
         unused(b);
     }
 
-    bool operator == (b <0> b) {
+    bool operator==(b<0> b)
+    {
         unused(b);
         return true;
     }
-    bool operator != (b <0> b) {
+    bool operator!=(b<0> b)
+    {
         unused(b);
         return false;
     }
 
-    friend ostream & operator << (ostream & os, b<0> b) {
+    friend ostream &operator<<(ostream &os, b<0> b)
+    {
         unused(b);
         return os;
     }
-    ostream & print_bool(ostream & os) {
+    ostream &print_bool(ostream &os)
+    {
         return os;
     }
-    ostream & print_string(ostream & os, bool is_value = true) {
+    ostream &print_string(ostream &os, bool is_value = true)
+    {
         unused(is_value);
-        os << ".";
         return os;
     }
+
+    b<0> operator|(b<0> b) { unused(b); return this; }
+    b<0> operator&(b<0> b) { unused(b); return this; }
+    b<0> operator!() { return this; }
 };
 
 //'if constexpr' only available with '-std=c++17' or '-std=gnu++17'
 
 template <int X, int Z>
-ostream& print_all_possibilities_tmp(ostream& os, b<X> cur) {
-    if constexpr (X < Z) {
-        print_all_possibilities_tmp<X + 1, Z>(os, b<X+1>(cur, true));
-        print_all_possibilities_tmp<X + 1, Z>(os, b<X+1>(cur, false));
-    } else
+ostream &print_all_possibilities_tmp(ostream &os, b<X> cur)
+{
+    if constexpr (X < Z)
     {
-      os << cur << endl;
+        print_all_possibilities_tmp<X + 1, Z>(os, b<X + 1>(cur, true));
+        print_all_possibilities_tmp<X + 1, Z>(os, b<X + 1>(cur, false));
+    }
+    else
+    {
+        os << cur << endl;
     }
     return os;
 }
 
 template <int X = 0, int Z>
-ostream& print_all_possibilities(ostream& os, b<X> cur)
+ostream &print_all_possibilities(ostream &os, b<X> cur)
 {
-    print_all_possibilities_tmp<X,Z>(os, cur);
+    print_all_possibilities_tmp<X, Z>(os, cur);
     os << endl;
     return os;
 }
 
-int main() {
+template <int X, int Z>
+void get_all_possibilities(vector<b<Z>>& vals, b<X> cur)
+{
+    if constexpr (X < Z)
+    {
+        get_all_possibilities<X + 1, Z>(vals, b<X + 1>(cur, true));
+        get_all_possibilities<X + 1, Z>(vals, b<X + 1>(cur, false));
+    }
+    else
+    {
+        vals.push_back(cur);
+    }
+    //return vals;
+}
 
-    print_all_possibilities<0,1>(cout, b<0>());
-    print_all_possibilities<0,2>(cout, b<0>());
-    print_all_possibilities<0,3>(cout, b<0>());
-    print_all_possibilities<0,4>(cout, b<0>());
+template<int X>
+void print_operator()
+{
+    vector<b<X> > v = vector<b<X> >();
+    get_all_possibilities<0,X>(v, b<0>()); 
+    string sep = "   ";
+
+    cout << "Bool of " << X << " orders :"  << endl;
+
+    cout << "================= (!a) =========" << endl;
+
+    for (auto &a : v)
+    {
+        cout << "(a)=(" << a << ")" << sep << "(!a)=(" << (!a) << ")" << endl;
+    }
+    cout << endl;
+
+    cout << "================= (a & b) =========" << endl;
+    for (auto &a : v)
+    {
+        for (auto &b : v)
+        {
+            cout << "(a)=(" << a << ")" << sep <<
+                    "(b)=(" << b << ")" << sep << ":" << sep <<
+                    "(a & b)=(" << (a & b)<< ")"  << endl;
+        }
+    }
+    cout << endl;
+
+    cout << "================= (a | b) =========" << endl;
+    for (auto &a : v)
+    {
+        for (auto &b : v)
+        {
+            cout << "(a)=(" << a << ")" << sep <<
+                    "(b)=(" << b << ")" << sep << ":" << sep <<
+                    "(a | b)=(" << (a | b)<< ") "  << endl;
+        }
+    }
+    cout << endl;
+
+    cout << "================= (a => b) =========" << endl;
+    for (auto &a : v)
+    {
+        for (auto &b : v)
+        {
+            cout << "(a)=(" << a << ")" << sep <<
+                    "(b)=(" << b << ")" << sep << ":" << sep <<
+                    "((a&b) | (!a) | b)=(" << ((a&b) | (!a) | b) << ") " << endl; /* sep <<
+                    "((a&b) | (!a))=(" << ((a&b) | (!a)) << ") " << sep <<
+                    "((a&b) | ((!a)&b) | ((!a)&(!b)))=(" << ((a&b) | ((!a)&b) | ((!a)&(!b))) << ") " << sep <<
+                    "((!a) | b)=(" << ((!a) | b)<< ") " << endl;*/
+        }
+    }
+    cout << endl;
+
+}
+
+int main()
+{
+
+    /*
+    cout << b<0> () << endl << endl;
+
+    cout << b<1> (true) << endl;
+    cout << b<1> (false) << endl << endl;
+
+    cout << b<2> (true , true ) << endl;
+    cout << b<2> (false, true ) << endl;
+    cout << b<2> (true , false) << endl;
+    cout << b<2> (false, false) << endl << endl;
+
+    cout << b<3> (b<2> (true , true ), true ) << endl;
+    cout << b<3> (b<2> (false, true ), true ) << endl;
+
+    cout << b<3> (b<2> (true, false ), true ) << endl;
+
+    cout << b<3> (b<2> (false, false), false) << endl;
+    cout << b<3> (b<2> (false, true ), false) << endl;
+    */
+
+    // print_all_possibilities<0,1>(cout, b<0>());
+    // print_all_possibilities<0,2>(cout, b<0>());
+    // print_all_possibilities<0,3>(cout, b<0>());
+    // print_all_possibilities<0,4>(cout, b<0>());
+    // print_all_possibilities<0,5>(cout, b<0>());
+    // print_all_possibilities<0,6>(cout, b<0>());
+
+    //print_all_possibilities
+
+    //cout << (b<1>(true) & b<1>(true)) << endl;
+
+    print_operator<0>();
+    print_operator<1>();
+    print_operator<2>();
+    print_operator<3>();
 
     return 0;
 }
